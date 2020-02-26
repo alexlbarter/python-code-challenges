39e11448c05fe2404896e1055c38f3fa373c19cd
alexlbarter <31287542+alexlbarter@users.noreply.github.com>
2020-02-26 13:28:46 +0000

    Added code and config files

---
+++ add_digits.py
@@ -0,0 +1,11 @@
+def sum_number_digits(number):
+    def split_digits(num):
+        return [int(x) for x in str(num)]
+
+    def sum_digits(digits):
+        return sum(digits)
+
+    while len(str(number) > 1):
+        number = sum_digits(split_digits(number))
+
+    return number
