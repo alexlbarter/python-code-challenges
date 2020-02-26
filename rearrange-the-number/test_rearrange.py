861af223eab71df842f378f52afee053dc43ee9a
alexlbarter <31287542+alexlbarter@users.noreply.github.com>
2020-02-26 17:33:22 +0000

    Fixed comment

--- rearrange-the-number/test_rearrange.py
+++ rearrange-the-number/test_rearrange.py
@@ -1,12 +1,18 @@
 import unittest
 import rearrange
 
+
 class TestRearrange(unittest.TestCase):
 
     def test_rearrange(self):
-        # Test one digit numbers
+        # Test 1 digit numbers
         for i in range(10):
             self.assertEqual(rearrange.rearrange(i), 0)
+
+        # Test larger numbers
+        cases = ((972882, 760833), (3320707, 7709823), (90010, 90981))
+        for case in cases:
+            self.assertEqual(rearrange.rearrange(case[0]), case[1])
 
 
 if __name__ == '__main__':
