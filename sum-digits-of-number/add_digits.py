16269019f73e61e95aa43c4c384fc5bc7d31d88d
alexlbarter <31287542+alexlbarter@users.noreply.github.com>
2020-02-26 17:00:36 +0000

    Fixed error strings

--- sum-digits-of-number/add_digits.py
+++ sum-digits-of-number/add_digits.py
@@ -1,11 +1,11 @@
 def sum_number_digits(number):
     try:
         if number < 0:
-            raise ValueError("{} is not positive")
+            raise ValueError("{} is not positive".format(number))
         if number % 1 != 0:
-            raise ValueError("{} is not an integer")
+            raise ValueError("{} is not an integer".format(number))
     except TypeError:
-        raise ValueError("{} is not a number")
+        raise ValueError("{} is not a number".format(number))
 
     def split_digits(num):
         return [int(x) for x in str(num)]
