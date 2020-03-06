59270742f8d87d1309367e73ab063692ba5fb340
alexlbarter <31287542+alexlbarter@users.noreply.github.com>
2020-03-06 12:38:09 +0000

    Rename test class

--- primitive-pythagorean-triples/test_primitive.py
+++ primitive-pythagorean-triples/test_primitive.py
@@ -2,7 +2,7 @@ import unittest
 import primitive
 
 
-class MyTestCase(unittest.TestCase):
+class TestPrimitive(unittest.TestCase):
     def test_primitive(self):
         cases = (([4, 5, 3], True), ([7, 12, 13], False), ([39, 15, 36], False), ([77, 36, 85], True))
         for case in cases:
